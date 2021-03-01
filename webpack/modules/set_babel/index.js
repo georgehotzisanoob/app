3e3e490dca6803b1ad7f88a094cd28dae24ba0f6
Tyler Durden <magnuswebdevcarlsen@gmail.com>
2021-03-01 04:21:17 +0300

    lint

--- webpack/modules/set_babel/index.js
+++ webpack/modules/set_babel/index.js
@@ -3,7 +3,7 @@ export const setBabel = () => {
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.js?x$/,
           use: "babel-loader",
           exclude: /(node_modules|bower_components)/,
         },
